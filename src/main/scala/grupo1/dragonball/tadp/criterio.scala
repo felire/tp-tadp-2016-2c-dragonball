dcea2431095f2033894df38b37b60de88a58bbe0
santipecin <santi_pecin@hotmail.com>
2016-11-16 23:02:50 -0300

    subo cambio en criterios, falta todavia

--- src/main/scala/grupo1/dragonball/tadp/criterio.scala
+++ src/main/scala/grupo1/dragonball/tadp/criterio.scala
@@ -6,37 +6,38 @@ trait Criterio{
 
 case object oponentesDebiles extends Criterio{
   def apply(atacante:Guerrero,atacado:Guerrero, movimiento:Movimiento): Option[Double]   = {
-    Peleando(atacante,atacado).flatMap(movimiento) match{
+    movimiento(atacante,atacado) match{
       case Peleando(_,atacadoDespues)=> Some(atacado.energia - atacadoDespues.energia)
       case _ => None
-    }    
+    }
   }
 }
 
 case object oponentesFuertes extends Criterio{
   def apply(atacante:Guerrero,atacado:Guerrero, movimiento:Movimiento): Option[Double]   = {
-    Peleando(atacante,atacado).flatMap(movimiento) match{
-      case Peleando(_, atacadoDespues) => Some(atacadoDespues.energia - atacado.energia)
+    movimiento(atacante,atacado) match{
+      case Peleando(_, atacadoDespues) => Some(atacadoDespues.energia)
       case _ => None
-    }    
+    }
   }
 }
 
 case object tacanio extends Criterio{
   def apply(atacante:Guerrero,atacado:Guerrero, movimiento:Movimiento): Option[Double]   = {
-    Peleando(atacante,atacado).flatMap(movimiento) match{
-      case Peleando(atacanteDespues,_) => Some(atacante.items.size - atacanteDespues.items.size)
+    movimiento(atacante,atacado) match{
+      case Peleando(atacanteDespues,_) => Some(atacanteDespues.items.size)
       case _ => None
-    }    
+    }
   }
 }
 
-
+//TODO hacer el que no lo mate tome en cuenta el contra ataque y que gane el atacante
 case object queNoLoMate extends Criterio{
   def apply(atacante:Guerrero,atacado:Guerrero, movimiento:Movimiento): Option[Double]   = {
     Peleando(atacante,atacado).flatMap(movimiento) match{
-      case Peleando(_,atacadoDespues)=> Some(atacadoDespues.energia) //mientras mas energia tiene mejor
+      case Peleando(atacanteDespues,_) if (atacanteDespues.energia == 0) => None
+      case Peleando(_,_) => Some(1)
       case _ => None
-    }    
+    }
   }
 }
