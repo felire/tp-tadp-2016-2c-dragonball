3f1f2a0774b27fffe367418f640d04b5e6339679
santipecin <santi_pecin@hotmail.com>
2016-11-16 22:13:41 -0300

    criterio retornan Option en lugar de 0 dan None es mas entendible el tema de que retorna si falla o si anda bien

--- src/main/scala/grupo1/dragonball/tadp/criterio.scala
+++ src/main/scala/grupo1/dragonball/tadp/criterio.scala
@@ -1,45 +1,42 @@
 package grupo1.dragonball.tadp
 
 trait Criterio{
-  def apply(atacante:Guerrero,atacado:Guerrero,movimiento:Movimiento): Double    
+  def apply(atacante:Guerrero,atacado:Guerrero,movimiento:Movimiento): Option[Double]    
 }
 
 case object oponentesDebiles extends Criterio{
-  def apply(atacante:Guerrero,atacado:Guerrero, movimiento:Movimiento): Double = {
+  def apply(atacante:Guerrero,atacado:Guerrero, movimiento:Movimiento): Option[Double]   = {
     movimiento(atacante,atacado) match{
-      case Peleando(_,atacadoDespues)=>atacado.energia-atacadoDespues.energia
-      case _ =>0
+      case Peleando(_,atacadoDespues)=> Some(atacado.energia - atacadoDespues.energia)
+      case _ => None
     }    
   }
 }
 
 case object oponentesFuertes extends Criterio{
-  def apply(atacante:Guerrero,atacado:Guerrero, movimiento:Movimiento): Double = {
+  def apply(atacante:Guerrero,atacado:Guerrero, movimiento:Movimiento): Option[Double]   = {
     movimiento(atacante,atacado) match{
-      case Peleando(_, atacadoDespues) if (atacado.energia == atacadoDespues.energia) => 1
-      case Peleando(_, atacadoDespues) => (-1)*(atacado.energia-atacadoDespues.energia)
-      case _ =>0
+      case Peleando(_, atacadoDespues) => Some(atacadoDespues.energia - atacado.energia)
+      case _ => None
     }    
   }
 }
 
 case object tacanio extends Criterio{
-  def apply(atacante:Guerrero,atacado:Guerrero, movimiento:Movimiento): Double = {
+  def apply(atacante:Guerrero,atacado:Guerrero, movimiento:Movimiento): Option[Double]   = {
     movimiento(atacante,atacado) match{
-      case Peleando(atacanteDespues,_) if (atacante.items.size == atacanteDespues.items.size) => 1
-      case Peleando(atacanteDespues,_) => (atacante.items.size - atacanteDespues.items.size)
-      case _ =>0
+      case Peleando(atacanteDespues,_) => Some(atacante.items.size - atacanteDespues.items.size)
+      case _ => None
     }    
   }
 }
 
 
 case object queNoLoMate extends Criterio{
-  def apply(atacante:Guerrero,atacado:Guerrero, movimiento:Movimiento): Double = {
+  def apply(atacante:Guerrero,atacado:Guerrero, movimiento:Movimiento): Option[Double]   = {
     movimiento(atacante,atacado) match{
-      case Peleando(_,_)=>1
-      case Ganador(_) =>0
-      case _ =>(-1)
+      case Peleando(_,atacadoDespues)=> Some(atacadoDespues.energia) //mientras mas energia tiene mejor
+      case _ => None
     }    
   }
 }
